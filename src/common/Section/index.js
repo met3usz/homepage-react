963a60f610dad71871697e087e7b22d10b33b310
Mateusz <m.wasik1303@gmail.com>
2023-03-04 10:47:53 +0100

    Added box-shadow and border radius

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -5,4 +5,7 @@ export const Section = styled.section`
   padding: 32px;
   margin: 72px auto;
   background: ${({ theme }) => theme.color.white};
+  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
+    0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
 `;
