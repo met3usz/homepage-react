2d4dcba810b049b407879333792806ebe45348fa
Mateusz <m.wasik1303@gmail.com>
2023-03-04 09:32:37 +0100

    Reworked grid columns

--- src/common/StyledList/styled.js
+++ src/common/StyledList/styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ListWrapper = styled.section`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(min(115px), 1fr));
   gap: 115px;
 `;
 
